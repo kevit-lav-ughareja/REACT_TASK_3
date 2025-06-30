10988fa6854783af2c3767551b7fe76c1f358f07
kevit-lav-ughareja <lav.ughareja@botprosolutions.com>
2025-06-30 12:51:47 +0530

    remove some unnecessary file

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Signup.css";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function Signup() {
